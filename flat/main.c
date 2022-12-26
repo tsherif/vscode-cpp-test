10982e77ad81c939dfc6d31a9fd0b09275884d66
Tarek Sherif <tsherif@gmail.com>
2022-12-26 17:02:04 -0500

    Initial

---
+++ flat/main.c
@@ -0,0 +1,5 @@
+#include "lib.h"
+
+void main(void) {
+    printNum(5);
+}
---
+++ nested/lib/lib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "lib.h"
+
+void printNum(int num) {
+    printf("The number is %d\n", num);
+}
